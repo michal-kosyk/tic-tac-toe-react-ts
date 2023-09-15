a3b7bad5fde53d2dfa5ac8cf36101290953bbf04
Michał Kosyk <michalkosyk@MacBook-Pro-Micha.local>
2023-09-15 17:16:10 +0200

    Add data- identifiers to html structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 const App = {
   $: {
-    menu: document.querySelector(".menu"),
-    menuButton: document.querySelector(".menu-btn"),
-    menuItems: document.querySelector(".items"),
+    menu: document.querySelector("[data-id='menu']"),
+    menuButton: document.querySelector("[data-id='menu-btn']"),
+    menuItems: document.querySelector("[data-id='menu-items']"),
+    resetBtn: document.querySelector("[data-id='reset-btn']"),
+    newRoundBtn: document.querySelector("[data-id='new-round-btn']"),
+    squares: document.querySelectorAll("[data-id='square']"),
   },
 
   init() {
+    this.registerEventListeners();
+  },
+
+  registerEventListeners() {
+    // DONE
     App.$.menuButton.addEventListener("click", (event) => {
       App.$.menuItems.classList.toggle("hidden");
+    });
+
+    // TODO
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset round!");
+    });
+
+    // TODO
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("New Round!");
+    });
+
+    // TODO
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(event.target.id);
+      });
     });
   },
 };
