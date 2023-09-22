486d729eba56c475c707b3b2bba7ec0e6cb4d510
Michał Kosyk <michalkosyk@MacBook-Pro-Micha.local>
2023-09-22 14:40:06 +0200

    Finish basic configuration of babel and webpack

--- react/src/index.jsx
+++ react/src/index.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
 const rootElement = document.getElementById("react-root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <p>Hello World, React!</p>
+    <App />
   </StrictMode>
 );
