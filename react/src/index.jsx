6682f0e1de3d32bd250ce401ba1dcb3d312ce2c6
Michał Kosyk <michalkosyk@MacBook-Pro-Micha.local>
2023-09-22 14:25:44 +0200

    scaffold a basic react app with webpack and babel

---
+++ react/src/index.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("react-root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <p>Hello World, React!</p>
+  </StrictMode>
+);
