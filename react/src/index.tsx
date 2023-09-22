49a126c39470f9bad03da5a12332cfaba78cf186
Michał Kosyk <michalkosyk@MacBook-Pro-Micha.local>
2023-09-22 15:14:54 +0200

    Initialize typescript

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 
 const rootElement = document.getElementById("react-root");
+if (!rootElement) throw new Error("RootElementNotFound");
 const root = createRoot(rootElement);
 
 root.render(
